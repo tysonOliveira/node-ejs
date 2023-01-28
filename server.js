ecdd03e8aef31afe3fdb37da933380fb307cc3f6
tysonOliveira <thaisonoliveira@gmail.com>
2023-01-28 12:57:50 -0300

    render the data on the page with foreach

--- server.js
+++ server.js
@@ -5,7 +5,27 @@ const app = express();
 app.set("view engine", "ejs");
 
 app.get("/", function (req, res) {
-  res.render("pages/index");
+  const items = [
+    {
+      title: "jfl",
+      message: "Lorem ipsum dolor sit amet consectetur adipisicing"
+    },
+    {
+      title: "dsçpf",
+      message: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quaerat esse maiores quasi similique laudantium sit"
+    },
+    {
+      title: "euro",
+      message: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quaerat esse maiores quasi similique laudantium sit est numquam voluptas vero blanditiis laboriosam saepe incidunt"
+    },
+    {
+      title: "diie",
+      message: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quaerat esse maiores quasi similique laudantium sit est numquam voluptas vero blanditiis laboriosam saepe incidunt ab, consequuntur voluptatibus! Non doloribus nesciunt cum!"
+    }
+  ];
+  res.render("pages/index", {
+    qualitys: items,
+  });
 })
 
 app.get("/sobre", function (req, res) {
@@ -14,3 +34,4 @@ app.get("/sobre", function (req, res) {
 
 app.listen(8080);
 console.log("Rodando");
+
